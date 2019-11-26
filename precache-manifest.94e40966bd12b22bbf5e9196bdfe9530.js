f3939e71250d06b5f8961f3473401b04652b0631
José Rosario-López <jose@sooryen.com>
2019-11-26 03:06:30 -0500

    Updates

---
+++ precache-manifest.94e40966bd12b22bbf5e9196bdfe9530.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15207d4d9fae21ff34092d62964a7e2b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "516f35263fa198b8acbf",
+    "url": "/monsters-rolodex/static/css/main.13b4f2c6.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "516f35263fa198b8acbf",
+    "url": "/monsters-rolodex/static/js/main.bb923f8d.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
